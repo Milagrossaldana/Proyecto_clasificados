4767604db27ec49a330763066068f7940de745d9
jpchechi <juanpichechi@gmail.com>
2022-05-09 21:39:18 -0300

    PRIMERO COMMIT DE MASTER

---
+++ src/main/java/arg/cuarteto/Proyecto_clasificados/ProyectoClasificadosApplication.java
@@ -0,0 +1,13 @@
+package arg.cuarteto.Proyecto_clasificados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoClasificadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoClasificadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arg/cuarteto/Proyecto_clasificados/ProyectoClasificadosApplicationTests.java
@@ -0,0 +1,13 @@
+package arg.cuarteto.Proyecto_clasificados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoClasificadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
